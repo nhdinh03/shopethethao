2dc77d8c53a72a737b97642d4b12936d50abc67c
nhdinh03 <nhdinhpc03@gmail.com>
2025-01-16 16:01:57 +0700

    Cau hinh fe

--- product-shopthethao/ShopeTheThao_frontend_react/src/index.jsx
+++ product-shopthethao/ShopeTheThao_frontend_react/src/index.jsx
@@ -1,38 +1,16 @@
-import React from "react";
-
-const globalStyles = {
-  body: {
-    margin: 0,
-    fontFamily:
-      "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', " +
-      "'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-    WebkitFontSmoothing: "antialiased",
-    MozOsxFontSmoothing: "grayscale",
-  },
-  code: {
-    fontFamily:
-      "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace",
-  },
-};
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.jsx';
+import App from './App.jsx';
+import reportWebVitals from './reportWebVitals.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+    <App />
+  
+);
 
-
-const App = () => {
-  // Apply body styles on mount
-  React.useEffect(() => {
-    Object.assign(document.body.style, globalStyles.body);
-  }, []);
-
-  return (
-    <div>
-      <p>
-        
-        Edit <code style={globalStyles.code}>src/App.js</code> and save to
-        reload.
-      </p>
-    </div>
-  );
-};
-
-export default App;
+reportWebVitals();
